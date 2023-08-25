e38f5cc38320682ab00456fe7e644a325eb35a82
HowToPlayMeow <gamebossmc682@gmail.com>
2023-08-25 11:10:49 +0700

    code v.2 : Moew

--- PY.py
+++ PY.py
@@ -1,1 +1,7 @@
 print('Hi Meow')
+print(123.5620)
+print(True)
+print(False)
+print(456+456)
+# \ -> \n, \t, \", \', \\, \b
+print('AAA\nbbb\t\tcc\ndddf\"adas\"')
---
+++ PY2.py
@@ -0,0 +1,9 @@
+print("Hi meow",456,'meow',True,10+20,":3")
+print("Hi meow"+str(456)+'meow'+str(True)+str(10+20)+":3")
+print("Hi meow "+str(456)+' meow '+str(True)+' '+str(10+20)+" :3")
+print("Hi meow {} meow {} {} :3".format(456,True,10+20))
+print("Hi meow {0} meow {1} {2} :3".format(456,True,10+20))
+                                        #   0    1    2
+print(f"Hi meow {456} meow {True} {10+20} :3")
+# % -> %d, %f, %c, %s, %i
+print('Hi meow %d meow %s %d :3' %(456,True,10+20))
